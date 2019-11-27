05cbc711d3f08baaaa8d7b64ebb56bf300e1f347
Breyanna Vass <breyannavass@gmail.com>
2019-11-27 16:55:06 -0500

    completed the text field file

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 interface Person {
   firstName: string;
@@ -11,13 +11,21 @@ interface Props {
   i?: number;
   fn?: (bob: string) => string;
   person: Person;
-  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const TextField: React.FC<Props> = () => {
+interface TextNode {
+  text: string;
+}
+
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+  const [count, setCount] = useState<TextNode>({ text: "hello" });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      <input />
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
     </div>
   );
 };
